795a8464d7b15f0e0b67aecdd5f6cda87946d8e9
zhaolei <zhaolei@i1602.com>
2016-01-22 13:34:23 +0800

    array

--- src/ndarray/nd01.py
+++ src/ndarray/nd01.py
@@ -1,9 +1,12 @@
 import mxnet as mx
 a = mx.nd.zeros((100, 50))
-
 b = mx.nd.ones((256, 32, 128, 1))    
-
 c = mx.nd.array([[1, 2, 3], [4, 5, 6]])    
-
 print a.asnumpy()   
 
+a = mx.nd.zeros((100, 50))
+b = mx.nd.ones((100, 50))
+
+print a.shape    
+
+c = a+b
