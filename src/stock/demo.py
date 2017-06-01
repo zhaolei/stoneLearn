3e53a9694f0b3f5be7a5d7b305e9609654c1d375
zhaolei <zhaolei@i1602.com>
2017-06-01 12:04:17 +0800

    stock check

--- src/stock/demo.py
+++ src/stock/demo.py
@@ -1,4 +1,16 @@
 import quandl
-Hos = quandl.get("WIKI/HOS",start_date = '2010-01-01', end_date = '2017-04-14')
+import pandas as pd
+quandl.ApiConfig.api_key = "GRFAUD2HY43XdKsfj8Az"
+#Hos = quandl.get("WIKI/HOS",start_date = '2010-01-01', end_date = '2017-04-14')
+Hos = quandl.get("WIKI/BIDU",start_date = '2012-01-01', end_date = '2017-04-14')
 
+print Hos.values.shape
 print Hos.head(5)
+print dir(Hos)
+print Hos.columns
+#print Hos.Close
+print Hos.values[10][0]
+Hos.to_pickle('bidut')
+fh = pd.read_pickle('bidut')
+print fh.values.shape
+print fh.columns
